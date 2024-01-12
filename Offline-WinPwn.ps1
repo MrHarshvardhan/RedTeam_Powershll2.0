9860efd9480d8b11b20fa0a2c60c65388a9ab6fd
alert('0-0') <49817411+MrHarshvardhan@users.noreply.github.com>
2024-01-12 18:08:40 +0530

    Update Offline-WinPwn.ps1

--- Offline-WinPwn.ps1
+++ Offline-WinPwn.ps1
@@ -1,5 +1,3 @@
 https://drive.google.com/file/d/1ME-yjjmNJxvDs29JtpRIm0nb-kJ_wkzA/view?usp=sharing
 
 https://drive.google.com/file/d/1gB-VucjHSZBtU_06iVue_ZKdTxFJx9Eg/view?usp=sharing
-
-https://chat.openai.com/share/c813f86e-ab5b-42a7-a4cd-b6841a5d2214
